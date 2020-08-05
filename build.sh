175947fa7f1edda3001cfbe101e8829b79d56ec1
shortpoet <soriano.carlitos@gmail.com>
2020-08-05 11:22:21 -0500

    update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/colors.cfg"
-echo -e "${Cyan}The ${Yellow}kanban ${Cyan}build script has been executed${NC}"
+echo -e "${Cyan}The ${Yellow}vite-ts-docker ${Cyan}build script has been executed${NC}"
 
 docker-compose build --force-rm --no-cache
